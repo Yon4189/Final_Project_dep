ad965eabc15630e8bedcb8c4da0ee1ac4a80547e
Yon4189 <animawabate@gmail.com>
2026-02-14 00:09:14 +0300

    run migration file

--- backend/database/migrations/2026_02_07_085217_fix_is_verified_default_value_in_serviceproviders_table.php
+++ backend/database/migrations/2026_02_07_085217_fix_is_verified_default_value_in_serviceproviders_table.php
@@ -8,14 +8,14 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::table('serviceproviders', function (Blueprint $table) {
+        Schema::table('service_providers', function (Blueprint $table) {
             $table->boolean('isVerified')->nullable()->default(null)->change();
         });
     }
 
     public function down()
     {
-        Schema::table('serviceproviders', function (Blueprint $table) {
+        Schema::table('service_providers', function (Blueprint $table) {
             $table->boolean('isVerified')->nullable(false)->default(0)->change();
         });
     }
