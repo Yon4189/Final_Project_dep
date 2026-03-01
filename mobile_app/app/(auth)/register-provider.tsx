5eb99ff712aca0b95c9c7457941cec9c10984b33
Yoseph <yositilahun21@gmail.com>
2026-03-01 15:16:43 +0300

    gfhhg

--- mobile_app/app/(auth)/register-provider.tsx
+++ mobile_app/app/(auth)/register-provider.tsx
@@ -709,7 +709,6 @@ export default function RegisterProviderScreen() {
         <View style={styles.uploadContainer}>
           <Text style={styles.label}>
             <Text>Business License/Certificate </Text><Text style={styles.required}>*</Text>
-            <Text>what shall I do </Text><Text style={styles.required}>*</Text>
           </Text>
           <TouchableOpacity style={styles.idImagePicker} onPress={() => pickImage('credential')}>
             {credentialPhotoUri ? (
